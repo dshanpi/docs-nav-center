100b9b44e7299099395d31ac33c32b3cadabf353
David Dahan <david.dahan3@gmail.com>
2025-08-25 16:36:24 +0000

    Change vueschool script

--- plugins/vueschool.client.ts
+++ plugins/vueschool.client.ts
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin(() => {
   const script = document.createElement("script")
-  script.src = "https://vueschool.io/banner.js?affiliate=UILIB&type=top"
+  script.src = "https://media.bitterbrains.com/main.js?from=UILIB&type=top"
   script.async = true
   document.head.appendChild(script)
 })
